9b2ef77f68eef97d9d409fb2d3dcbe480685ad17
kip2 <tqa1no4@gmail.com>
2023-11-26 20:52:20 +0900

    トップ画面の要素だけ配置

--- clicker-game/src/components/page/Top/Top.tsx
+++ clicker-game/src/components/page/Top/Top.tsx
@@ -1,8 +1,25 @@
+import "./Top.css"
+import Button from "../../ui/Button"
 
 const Top = () => {
     return(
         <>
-            <h1>Top画面</h1>
+            <div>
+                <h1>せっかち夢十夜第一夜</h1>
+            </div>
+            <div>
+                <Button text="始める"></Button>
+                <Button text="あらすじ"></Button>
+                <Button text="遊び方"></Button>
+            </div>
+            <div>
+                <p>
+                    ユーザー名: ユーザー1
+                </p>
+                <p>
+                    ユーザーID: 1111111
+                </p>
+            </div>
         </>
     )
 }
