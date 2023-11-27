b92b5d0be2805ce58013d2de297347b55ab24fae
kip2 <tqa1no4@gmail.com>
2023-11-27 20:44:40 +0900

    あらすじページに、画像とテキストを追加

--- clicker-game/src/components/page/Synopsis/Synopsis.tsx
+++ clicker-game/src/components/page/Synopsis/Synopsis.tsx
@@ -1,16 +1,35 @@
 import Button from "../../ui/Button"
+import "./Synopsis.css"
 
 // todo: 機能をあとで追加する
 const returnTop = () => {}
 
 const Synopsis = () => {
     return(
-        <>
+        <div className="synopsis-background">
+            <p>
+                女は静かな調子を一段張り上げて、「百年待っていて下さい」と思い切った声で云った。
+            </p>
+            <p>
+                「百年、私の墓の傍に坐って待っていてください。きっと逢いに来ますから」
+            </p>
+
+            <p>
+                男は云った。
+            </p>
+
+            <p>
+                「そんなに待てるか！」
+            </p>
+
+            <p>
+                生来のせっかちであった。
+            </p>
             <Button
                 text="戻る"
                 onButtonClick={returnTop}
             />
-        </>
+        </div>
     )
 }
 
