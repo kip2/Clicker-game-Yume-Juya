48399e64e45465c72a133d4a3fb2538ea259dfb7
kip2 <tqa1no4@gmail.com>
2023-11-28 21:04:16 +0900

    routerにより画面ごとのパスによる遷移を実装

--- clicker-game/src/App.tsx
+++ clicker-game/src/App.tsx
@@ -3,14 +3,19 @@ import Top from './components/page/Top/Top'
 import "./App.css"
 import Synopsis from "./components/page/Synopsis/Synopsis"
 import HowToPlay from "./components/page/HowToPlay/HowToPlay"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="app-background">
-      {/* <Top /> */}
-      {/* <Synopsis /> */}
-      <HowToPlay />
-    </div>
+    <>
+      <div className="app-background">
+        <Routes>
+          <Route path="/" element={<Top />}/>
+          <Route path="/synopsis" element={<Synopsis />}/>
+          <Route path="/howToPlay" element={<HowToPlay />}/>
+        </Routes>
+      </div>
+    </>
   )
 }
 
