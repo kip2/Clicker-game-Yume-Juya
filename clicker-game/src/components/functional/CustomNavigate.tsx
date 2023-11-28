5ae0a33d60b2bdc8235dae769b50fea9b2128269
kip2 <tqa1no4@gmail.com>
2023-11-28 21:25:19 +0900

    del: top用のnavigate関数を削除

--- clicker-game/src/components/functional/CustomNavigate.tsx
+++ clicker-game/src/components/functional/CustomNavigate.tsx
@@ -4,14 +4,10 @@ import { useNavigate } from "react-router-dom";
 export function  useCustomNavigate() {
     const navigate = useNavigate()
 
-    const navigateToTop = () => {
-        navigate("/")
-    }
-
     const navigateToPage = (path: string) => {
         navigate(path)
     }
 
-    return { navigateToPage, navigateToTop }
+    return { navigateToPage }
 }
 
