259d8c064ba5368dda51ffd08cfa90521d9c982d
kip2 <tqa1no4@gmail.com>
2023-11-29 21:30:40 +0900

    game画面の枠組みを配置

--- clicker-game/src/components/page/Game/Game.tsx
+++ clicker-game/src/components/page/Game/Game.tsx
@@ -4,7 +4,20 @@ function Game () {
     return (
         <>
             <div className="game-background">
-                <h1>Game</h1>
+                <div className="game-left-window">
+                    左
+                </div>
+                <div className="game-right-window">
+                    <div className="game-right-upper-window">
+                        右上
+                    </div>
+                    <div className="game-right-middle-window">
+                        アイテム
+                    </div>
+                    <div className="game-right-bottom-window">
+                        ボタン
+                    </div>
+                </div>
             </div>
         </>
     )
