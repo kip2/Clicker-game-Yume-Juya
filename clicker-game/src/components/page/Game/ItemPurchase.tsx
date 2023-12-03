884608a1a033b99d2713e8a1189259cd0ee583a8
kip2 <tqa1no4@gmail.com>
2023-12-03 14:12:29 +0900

    商品説明ページの大枠を作成

--- clicker-game/src/components/page/Game/ItemPurchase.tsx
+++ clicker-game/src/components/page/Game/ItemPurchase.tsx
@@ -21,9 +21,28 @@ function ItemPurchase ({
 }: ItemPurchaseProps) {
     return (
         <>
-            <h1>ItemPurchase</h1>
-            <div>
+            <h2> 購 入 細 目 </h2>
+            <div className="purchase-information">
+                <div className="purchase-information-description">
+                    <p>purchase</p>
+                </div>
+                <img
+                    className="purchase-image"
+                    src="./img/panamaHat.png"
+                ></img>
 
+            </div>
+            <input
+                className="purchase-input"
+                type="number"
+                min="1"
+                placeholder="幾つ購う？"
+            ></input>
+            <div className="display-price-box">
+                <p className="display-price-description">購入時の対価</p>
+                <p className="display-price-price">
+                    2000000 刻
+                </p>
             </div>
             <div className="purchase-information-buttons">
                 <Button 
