2114feef4fb6633ebe6ecaba4a3701706b934c4e
kip2 <tqa1no4@gmail.com>
2023-12-03 13:21:36 +0900

    購入画面に戻るボタンを設置

--- clicker-game/src/components/page/Game/Game.tsx
+++ clicker-game/src/components/page/Game/Game.tsx
@@ -63,6 +63,7 @@ function Game () {
                         ?
                             <ItemPurchase 
                                 {...item}
+                                backButton={handleItemBackButton}
                             />
                         :
                             <>
--- clicker-game/src/components/page/Game/ItemPurchase.tsx
+++ clicker-game/src/components/page/Game/ItemPurchase.tsx
@@ -1,16 +1,30 @@
+import Button from "../../ui/Button"
+
 type ItemPurchaseProps = {
     name: string
     remainingPurchaseQuantity: number
     reduceTime: number
     price: number
     description: string
+    backButton?: () => void
 }
 
 
-function ItemPurchase ({ name, remainingPurchaseQuantity, reduceTime, price, description}: ItemPurchaseProps) {
+function ItemPurchase ({ 
+    name, 
+    remainingPurchaseQuantity, 
+    reduceTime, 
+    price, 
+    description,
+    backButton
+}: ItemPurchaseProps) {
     return (
         <>
             <h1>ItemPurchase</h1>
+            <Button 
+                text="戻る"
+                onButtonClick={backButton}
+            />
         </>
     )
 
