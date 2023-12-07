fcac2fcd78e962e875f8ac4125a8227e99d3d976
kip2 <tqa1no4@gmail.com>
2023-12-07 21:29:08 +0900

    モーダルボタンで作成し直し

--- clicker-game/src/App.tsx
+++ clicker-game/src/App.tsx
@@ -5,6 +5,7 @@ import Synopsis from "./components/page/Synopsis/Synopsis"
 import HowToPlay from "./components/page/HowToPlay/HowToPlay"
 import { Routes, Route } from "react-router-dom"
 import Game from "./components/page/Game/Game"
+import SaveDataExistsModal from "./components/page/Modal/SaveDataExistsModal"
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/synopsis" element={<Synopsis />}/>
           <Route path="/howToPlay" element={<HowToPlay />}/>
           <Route path="/game" element={<Game />}/>
+          <Route path="/login/agree" element={<SaveDataExistsModal/>}/>
+          <Route path="/login/agree" element={<SaveDataExistsModal/>}/>
         </Routes>
       </div>
     </>
--- clicker-game/src/components/page/Modal/SaveDataExistsModal.tsx
+++ clicker-game/src/components/page/Modal/SaveDataExistsModal.tsx
@@ -1,8 +1,23 @@
+import ModalButton from "../../ui/ModalButton"
 
 function SaveDataExistsModal () {
     return (
         <>
-            <h1>Modal test!</h1>
+            <p>データの読み込みが完了しました</p>
+            <p>こちらのデータでゲームを開始しますか？</p>
+            <div>
+                <ModalButton
+                    text="ゲームを始める"
+                    onButtonClick={()=>{}}
+                />
+            </div>
+            <div>
+                <ModalButton
+                    text="別のゲームを登録する"
+                    onButtonClick={()=>{}}
+                />
+            </div>
+            <p>ユーザーデータを消去する</p>
         </>
     )
 }
