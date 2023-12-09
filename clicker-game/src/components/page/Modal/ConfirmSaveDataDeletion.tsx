b490d2ce48ba89b65d2624adfda8a91319815d22
kip2 <tqa1no4@gmail.com>
2023-12-09 20:36:56 +0900

    セーブデータ削除確認画面作成

--- clicker-game/src/components/page/Modal/ConfirmSaveDataDeletion.tsx
+++ clicker-game/src/components/page/Modal/ConfirmSaveDataDeletion.tsx
@@ -3,7 +3,21 @@ import ModalButton from "../../ui/ModalButton";
 function ConfirmSaveDataDeletion() {
     return(
         <>
-            <h1>Conrirm</h1>
+            <div className="blank-space"></div>
+            <p className="modal-text">ユーザーデータを消去しますか？</p>
+            <div className="blank-space"></div>
+            <div>
+                <ModalButton
+                    text="はい"
+                    onButtonClick={()=>{}}
+                />
+            </div>
+            <div>
+                <ModalButton
+                    text="いいえ"
+                    onButtonClick={()=>{}}
+                />
+            </div>
         </>
     )
 }
