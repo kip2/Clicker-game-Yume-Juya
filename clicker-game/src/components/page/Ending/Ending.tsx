88037a97174077b0decb383c7309604be2b84853
kip2 <tqa1no4@gmail.com>
2023-12-10 19:52:42 +0900

    Endingの演出を作成

--- clicker-game/src/components/page/Ending/Ending.tsx
+++ clicker-game/src/components/page/Ending/Ending.tsx
@@ -3,7 +3,15 @@ import "./Ending.css"
 function Ending () {
     return(
         <div className="ending-background">
-            <h1>ending</h1>
+            <div className="blank-space"></div>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '0s'}}>石の下から斜に自分の方へ向いて青い茎が伸びてきた。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '2s'}}>見る間に長くなってちょうど自分の胸の辺りまで来て留まった。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '4s'}}>と思うと、すらりと揺ぐ茎の頂に、心持首を傾けていた細長い一輪の蕾が、ふっくらと弁(はなびら)を開いた。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '6s'}}>真白な百合が鼻の先で骨に徹(こた)えるほど匂った。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '8s'}}>そこへ遥(はるか)の上から、ぽたりと露が落ちたので、花は自分の重みでふらふらと動いた。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '10s'}}>自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。</p>
+            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '12s'}}>自分が百合から顔を話す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。</p>
+            <p className="floating-text outlined-text ending-text-last" style={{ animationDelay: '16s'}}>「百年はもう来ていたんだな」とこの時初めて気がついた。</p>
         </div>
     )
 }
