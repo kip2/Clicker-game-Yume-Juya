c74dfcdde58f3e980d4cb872d0935567851a6491
kip2 <tqa1no4@gmail.com>
2023-12-10 20:02:52 +0900

    エンディングの演出完成

--- clicker-game/src/components/page/Ending/Ending.tsx
+++ clicker-game/src/components/page/Ending/Ending.tsx
@@ -12,6 +12,7 @@ function Ending () {
             <p className="floating-text outlined-text ending-text" style={{ animationDelay: '10s'}}>自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。</p>
             <p className="floating-text outlined-text ending-text" style={{ animationDelay: '12s'}}>自分が百合から顔を話す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。</p>
             <p className="floating-text outlined-text ending-text-last" style={{ animationDelay: '16s'}}>「百年はもう来ていたんだな」とこの時初めて気がついた。</p>
+            <p className="floating-text outlined-text-thanks ending-text-thanks" style={{ animationDelay: '18s'}}>Thank you for Playing!</p>
         </div>
     )
 }
