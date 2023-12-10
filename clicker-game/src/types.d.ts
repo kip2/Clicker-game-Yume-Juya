1a296b2350d5550e343518722a2c79d936ee9b98
kip2 <tqa1no4@gmail.com>
2023-12-10 13:47:37 +0900

    UserDataの型定義を追加

--- clicker-game/src/types.d.ts
+++ clicker-game/src/types.d.ts
@@ -6,3 +6,14 @@ type Item = {
     description: string,
     imgUrl: string
 }
+
+type UserData = {
+    name: string
+    money: number
+    decrementPerSecond: number
+    decrementPerClick: number
+    remainingTime: number
+    items: {
+        [key: string]: number
+    }
+}
