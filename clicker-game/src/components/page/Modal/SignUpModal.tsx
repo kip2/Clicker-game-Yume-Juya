ad6b4a3fab9d0c921fdd7147e6828db562647b57
kip2 <tqa1no4@gmail.com>
2023-12-10 13:29:54 +0900

    inputboxの値を制限＆valueのstateの管理を追加

--- clicker-game/src/components/page/Modal/SignUpModal.tsx
+++ clicker-game/src/components/page/Modal/SignUpModal.tsx
@@ -1,15 +1,20 @@
 import InputBox from "../../ui/InputBox"
 import ModalButton from "../../ui/ModalButton"
 import { ModalState } from "../../../Enum"
+import "./SignUpModal.css"
+import { useState } from "react"
 
 type SignUpModalProps = {
     setModalState : React.Dispatch<React.SetStateAction<ModalState>>
 }
 
 function SignUpModal({ setModalState }: SignUpModalProps) {
+    const [inputValue, setInputValue] = useState("")
+
     const handleUserRegistrationButton = () => {
         // todo: 実際にユーザーデータを登録してセーブする動作が必要
         // todo: データのfetchをさせるのも必要になる
+        
         setModalState(ModalState.ExistsSaveDataModal)
     }
 
@@ -17,8 +22,13 @@ function SignUpModal({ setModalState }: SignUpModalProps) {
         <>
             <div className="blank-space"></div>
             <p className="modal-text">名前を入力してください</p>
+            <p className="modal-cation-statement">※名前は30文字まで</p>
             <div className="blank-space"></div>
-            <InputBox />
+            <InputBox 
+                maxLength={30}
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+            />
             <div className="blank-space"></div>
             <ModalButton
                 text="登録する"
--- clicker-game/src/components/ui/InputBox.tsx
+++ clicker-game/src/components/ui/InputBox.tsx
@@ -1,11 +1,23 @@
 import "./InputBox.css"
 
-function InputBox() {
+type InputBoxProps = {
+    maxLength: number
+    inputValue: string
+    setInputValue:React.Dispatch<React.SetStateAction<string>>
+}
+
+function InputBox({ maxLength, inputValue, setInputValue }: InputBoxProps) {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
     return(
         <>
             <input 
                 className="input-box"
                 type="text"
+                maxLength={maxLength}
+                value={inputValue}
+                onChange={handleInputChange}
             >
             </input>
         </>
