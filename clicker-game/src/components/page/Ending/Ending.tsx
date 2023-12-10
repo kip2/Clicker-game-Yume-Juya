27a79b4e7ae1338a9a4f1b85e00ee0c4920dae31
kip2 <tqa1no4@gmail.com>
2023-12-10 20:05:29 +0900

    fix: class指定を間違えていたので修正

--- clicker-game/src/components/page/Ending/Ending.tsx
+++ clicker-game/src/components/page/Ending/Ending.tsx
@@ -4,14 +4,14 @@ function Ending () {
     return(
         <div className="ending-background">
             <div className="blank-space"></div>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '0s'}}>石の下から斜に自分の方へ向いて青い茎が伸びてきた。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '2s'}}>見る間に長くなってちょうど自分の胸の辺りまで来て留まった。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '4s'}}>と思うと、すらりと揺ぐ茎の頂に、心持首を傾けていた細長い一輪の蕾が、ふっくらと弁(はなびら)を開いた。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '6s'}}>真白な百合が鼻の先で骨に徹(こた)えるほど匂った。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '8s'}}>そこへ遥(はるか)の上から、ぽたりと露が落ちたので、花は自分の重みでふらふらと動いた。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '10s'}}>自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。</p>
-            <p className="floating-text outlined-text ending-text" style={{ animationDelay: '12s'}}>自分が百合から顔を話す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。</p>
-            <p className="floating-text outlined-text ending-text-last" style={{ animationDelay: '16s'}}>「百年はもう来ていたんだな」とこの時初めて気がついた。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '0s'}}>石の下から斜に自分の方へ向いて青い茎が伸びてきた。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '2s'}}>見る間に長くなってちょうど自分の胸の辺りまで来て留まった。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '4s'}}>と思うと、すらりと揺ぐ茎の頂に、心持首を傾けていた細長い一輪の蕾が、ふっくらと弁(はなびら)を開いた。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '6s'}}>真白な百合が鼻の先で骨に徹(こた)えるほど匂った。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '8s'}}>そこへ遥(はるか)の上から、ぽたりと露が落ちたので、花は自分の重みでふらふらと動いた。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '10s'}}>自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。</p>
+            <p className="floating-text ending-outlined-text ending-text" style={{ animationDelay: '12s'}}>自分が百合から顔を話す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。</p>
+            <p className="floating-text ending-outlined-text ending-text-last" style={{ animationDelay: '16s'}}>「百年はもう来ていたんだな」とこの時初めて気がついた。</p>
             <p className="floating-text outlined-text-thanks ending-text-thanks" style={{ animationDelay: '18s'}}>Thank you for Playing!</p>
         </div>
     )
