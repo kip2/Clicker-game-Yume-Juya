0e2de9f8a611c95975061878205fb3b7fd3624e4
kip2 <tqa1no4@gmail.com>
2023-12-16 22:24:53 +0900

    アプリ全体でUserDataを共有できるように変更

--- clicker-game/src/main.tsx
+++ clicker-game/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { GameProvider } from './components/model/GameContext.tsx'
+import { UserDataProvider } from './components/model/UserDataContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <GameProvider>
-        <App />
-      </GameProvider>
+      <UserDataProvider>
+        <GameProvider>
+          <App />
+        </GameProvider>
+      </UserDataProvider>
     </Router>
   </React.StrictMode>
 )
